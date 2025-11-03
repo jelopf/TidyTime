54c403fa7dbd4c591f6bda4f7f0feef49f68588d
jelopf <1nastyacha1@gmail.com>
2025-11-03 22:42:59 +0500

    Initial commit: TidyTime mobile app

---
+++ TidyTime/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TidyTime.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ TidyTime/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TidyTime.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
