0d15b1724b7563c4859a4384931441e94b5d6019
jelopf <1nastyacha1@gmail.com>
2025-12-13 16:46:25 +0500

    feat(roles): связаны ui и логика в профилях

--- TidyTime/ViewModels/ParentProfileViewModel.cs
+++ TidyTime/ViewModels/ParentProfileViewModel.cs
@@ -1,5 +1,8 @@
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using TidyTime.Models;
 using TidyTime.Services;
 using TidyTime.Views;
 
@@ -9,14 +12,58 @@ public partial class ParentProfileViewModel : ViewModelBase
 {
     private readonly IAuthService _authService;
     private readonly ITaskService _taskService;
+    private User? _currentUser;
+
+    [ObservableProperty]
+    private string _userName = "";
+
+    [ObservableProperty]
+    private string _userFullName = "";
+
+    [ObservableProperty]
+    private ObservableCollection<User> _children = new();
+
+    [ObservableProperty]
+    private bool _isLoading = true;
 
     public ParentProfileViewModel(INavigationService navigationService, IAuthService authService, ITaskService taskService) 
         : base(navigationService)
     {
         _authService = authService;
         _taskService = taskService;
+        _currentUser = authService.GetCurrentUser();
+
+        if (_currentUser != null)
+        {
+            UserName = _currentUser.Login;
+            UserFullName = _currentUser.FullName;
+        }
+        
+        _ = LoadChildrenAsync();
     }
 
+    private async Task LoadChildrenAsync()
+    {
+        if (_currentUser == null) return;
+        
+        try
+        {
+            IsLoading = true;
+            
+            var children = await _taskService.GetChildrenForParentAsync(_currentUser.Id);
+            
+            Children.Clear();
+            foreach (var child in children)
+            {
+                Children.Add(child);
+            }
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+    
     [RelayCommand]
     private void GoBack()
     {
@@ -28,5 +75,12 @@ public partial class ParentProfileViewModel : ViewModelBase
     private void GoToAuth()
     {
         NavigationService.NavigateTo(new AuthViewModel(NavigationService, _authService, _taskService));
+    }
+
+    [RelayCommand]
+    private async Task AddChild()
+    {
+        // TODO: Реализовать добавление ребенка
+        await LoadChildrenAsync();
     }
 }
